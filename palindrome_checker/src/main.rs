266c3b7469c287fda6f55ba84433b627cb5c3016
aixizu <67578661+aixizu@users.noreply.github.com>
2023-05-24 11:58:52 +0100

    🧨 - Oops...
    i spelt Palindrome wrong, that's a bit embarrassing
    
    - changed readme links to be based relative to tree rather than hard links
    - spell palindrome right mel...
    - changed comments to lowercase too cuz they were bothering me

--- palindrome_checker/src/main.rs
+++ palindrome_checker/src/main.rs
@@ -2,36 +2,36 @@ use std::io::{self, Write};
 
 fn is_palindrome(string: &str) -> bool
 {
-    // Create a new string to store the reversed version
+    // create a new string to store the reversed version
     // of the input string
     let mut reversed: String = String::new();
 
-    // Iterate over the characters in the input string in reverse order
+    // iterate over the characters in the input string in reverse order
     for char in string.chars().rev()
     {
-        // Append each character to the reversed string
+        // ppend each character to the reversed string
         reversed.push(char);
     }
 
-    // Compare the input string and reversed string
+    // compare the input string and reversed string
     string == reversed
 }
 
 fn main()
 {
-    // Prompt the user to enter a string and flush the line buffer 
+    // prompt the user to enter a string and flush the line buffer 
     // so it shows immediately!
     print!("Enter a string: ");
     io::stdout().flush().unwrap();
 
-    // Store the user's input
+    // store the user's input
     let mut input: String = String::new();
     io::stdin().read_line(&mut input).expect("Error reading line");
 
-    // Trim the user's input
+    // trim the user's input
     let palindrome: &str = input.trim();
 
-    // Check if the string is a palindrome
+    // check if the string is a palindrome
     if is_palindrome(palindrome) 
     {
         println!("{} is a palindrome", palindrome);
